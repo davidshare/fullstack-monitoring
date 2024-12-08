4b279981c0f5c6efe0f85c05a3563f2f64dc05e5
David Essien <davidessienshare@gmail.com>
2024-12-08 17:54:53 +0100

    update traefik to use http

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@ module "ec2_instance" {
   source                      = "github.com/davidshare/terraform-aws-modules//ec2?ref=ec2-v1.0.0"
 
   ami                         = "ami-0453ec754f44f9a4a"
-  instance_type               = "t2.micro"
+  instance_type               = "t3a.medium"
   key_name                    = "monitoring"
   subnet_id                   = module.subnets.id
   vpc_security_group_ids      = [module.security_group.id]
