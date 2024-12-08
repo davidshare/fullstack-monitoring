9bec39c74543e4bcd8806bcb424ea40ad0b3663b
David Essien <davidessienshare@gmail.com>
2024-12-08 04:50:19 +0100

    add project files

---
+++ todo-api/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.core.database import Base, engine
+from app.endpoints.v1.auth import router as auth_router
+
+# Initialize database
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Register routes
+app.include_router(auth_router)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the To-Do App API"}
---
+++ todo-api/main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from app.core.config import settings
+from app.core.database import engine
+from app.core.database import Base
+from app.endpoints.v1 import v1_router
+
+load_dotenv()
+
+# Initialize FastAPI app
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    description=settings.PROJECT_DESCRIPTION,
+    version=settings.PROJECT_VERSION
+)
+print("*************************", settings.FRONTEND_ORIGIN)
+origins = [
+    settings.FRONTEND_ORIGIN,  # Allow your frontend's origin
+    # Add other origins if needed
+]
+
+# Set up CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Adjust for your frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the FastAPI application!"}
+
+
+# Include API router
+app.include_router(v1_router, prefix=settings.API_V1_STR)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
